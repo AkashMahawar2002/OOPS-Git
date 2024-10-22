411a45607daa23c762e2fe919a6b6cfc857e1a4d
AkashMahawar2002 <142003846+AkashMahawar2002@users.noreply.github.com>
2024-10-22 08:34:53 +0530

    experiment_8a.cpp

--- experiment_8a.cpp
+++ experiment_8a.cpp
@@ -1,42 +1,87 @@
-#include<iostream>
-#include<string>
+#include <bits/stdc++.h>
 using namespace std;
 
-class employee{
-   string name;
-   int emp_id;
-   double salary;
-   public: 
-   Employee(){
-    name="";
-    emp_id=0;
-    salary=0.0;
-   }
+class Employee {
+    protected:
+    char name[20];
+    int empId;
+    double salary;
 
-   Employee(string name, int emp_id , double salary){
-    this->name=name;
-    this->emp_id=emp_id;
-    this->salary=salary;
-   }
-   void calculate_Monthly_Pay(){
+public:
+    Employee() {
+        empId = 0;
+        salary = 0.0;
+        strcpy(name, "Unknown");
+    }
 
-   }
+    void setDetails() {
+        cout << "Enter name, empId, and salary: ";
+        cin >> name >> empId >> salary;
+    }
 
+    void display() {
+        cout << "\nName: " << name
+             << "\nId: " << empId
+             << "\nSalary: " << salary;
+    }
+
+    virtual double monpay() {
+        return salary;
+    }
 };
 
-class employee : public professor{
+class Professor : public Employee {
+public:
+    double monpay() override {
+        return salary + salary * 0.1;
+    }
+};
+
+class Admin : public Employee {
+public:
+    double monpay() override {
+        return salary + salary * 0.05;
+    }
+};
+
+class Janitor : public Employee {
+    int workHour;
+
+public:
+    void setDetails() {
+        Employee::setDetails();
+        cout << "Enter work hours: ";
+        cin >> workHour;
+    }
+
+    double monpay() override {
+        return workHour * 500;
+    }
+};
+
+int main() {
+    Employee *e;
+    Professor p;
+    Admin a;
+    Janitor j;
     
-};
-
-class AdminStaff : public professor{
-
-};
-
-class Janitor : public professor{
-
-};
-
-int main(){
+    cout<<"for professor:";
+    e = &p;
+    e->setDetails();
+    e->display();
+    cout << "\nMonthly Pay: " << e->monpay() << endl;
     
+    cout<<"for admin:";
+    e = &a;
+    e->setDetails();
+    e->display();
+    cout << "\nMonthly Pay: " << e->monpay() << endl;
+    
+    cout<<"for janitor:";
+    e = &j;
+    e->setDetails();
+    e->display();
+    cout << "\nMonthly Pay: " << e->monpay() << endl;
+
+    return 0;
 }
-
